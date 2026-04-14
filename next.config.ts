607ead6f373323628844797134230e86d9adf010
Volkan-eles <vlkneles@gmail.com>
2026-04-14 15:18:16 +0300

    hotfix: remove infinite redirect loop for feed.xml

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: '/feed.xml',
-        destination: '/feed.xml/',
-        permanent: true,
-      },
-    ];
-  },
   async rewrites() {
     return [
       {
