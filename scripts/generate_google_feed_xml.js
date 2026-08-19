c9133f11834c3e326bcd9fd9171ac6141af63478
Volkan-eles <vlkneles@gmail.com>
2026-08-19 11:03:29 +0300

    fix(feed): add handling time and transit time tags for Google Merchant Center store quality optimization

--- scripts/generate_google_feed_xml.js
+++ scripts/generate_google_feed_xml.js
@@ -107,6 +107,10 @@ const items = products.map((product) => {
         <g:country>US</g:country>
         <g:service>Digital Download</g:service>
         <g:price>0.00 USD</g:price>
+        <g:min_handling_time>0</g:min_handling_time>
+        <g:max_handling_time>0</g:max_handling_time>
+        <g:min_transit_time>0</g:min_transit_time>
+        <g:max_transit_time>0</g:max_transit_time>
       </g:shipping>`;
 
   const adsRedirectXml = etsyUrl
